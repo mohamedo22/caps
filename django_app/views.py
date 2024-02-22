02828fe2fb26ac1bfc1c9de1bc3788674ee29ec0
mohamedo22 <oooomohamed421@gmail.com>
2024-02-22 13:19:31 +0200

    add

---
+++ django_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render , redirect , HttpResponseRedirect
+from .models import *
+from datetime import *
+from django.core.mail import send_mail
+from django.conf import settings
+def index(request):
+    return render(request, 'index.html')
