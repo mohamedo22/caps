37f3130161975ea0fb77c0c287c16762f2e18aa4
mohamedo22 <oooomohamed421@gmail.com>
2024-02-22 13:29:24 +0200

    update

--- django_app/urls.py
+++ django_app/urls.py
@@ -4,5 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from .views import *
 urlpatterns = [
-    path('' , index , name='index')
+    path('' , index , name='index'),
+    path('login/' , login , name='login'),
 ]
--- django_app/views.py
+++ django_app/views.py
@@ -5,3 +5,5 @@ from django.core.mail import send_mail
 from django.conf import settings
 def index(request):
     return render(request, 'index.html')
+def login(request):
+    return render(request, 'login.html')
