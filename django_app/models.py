06db9ae977a218725e49fc157806251dd0d080fd
mohamedo22 <oooomohamed421@gmail.com>
2024-02-24 00:46:39 +0200

    update

--- django_app/models.py
+++ django_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
-# Create your models here.
+class users(models.Model):
+    email=models.EmailField(max_length=254)
+    password=models.TextField()
+    def __str__(self) :
+        return self.email
+class admin(models.Model):
+    email=models.EmailField(max_length=254)
+    password=models.TextField()
+    def __str__(self) :
+        return self.email
+class flats(models.Model):
+    primarykey=models.IntegerField()
+    price = models.IntegerField()
+    bathroomnumber=models.IntegerField()
+    bedroomnumber=models.IntegerField()
+    gov = models.TextField()
+    city=models.TextField()
+    floornumber=models.IntegerField()
+    with_f = models.BooleanField()
+    active=models.BooleanField()
+    phone = models.IntegerField()
+class flat_images(models.Model):
+    flat = models.ForeignKey(flats, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='flat_images/')
+    
--- django_app/views.py
+++ django_app/views.py
@@ -6,4 +6,14 @@ from django.conf import settings
 def index(request):
     return render(request, 'index.html')
 def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        check = users.objects.get(email=email , password = password)
+        if check:
+            return redirect(home)
+        else:
+            return render(request, 'login.html' , {'check':'false'})
     return render(request, 'login.html')
+def home(request):
+    return render(request , 'home.html')
